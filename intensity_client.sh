16eb2a6d8f445b00ba951cb5a360d483268dc3d0
Alon Zakai <alon@sanctity.(none)>
2010-02-20 09:53:56 +0200

    bash scripts now chdir to their directory, for convenience (thanks BiosElement)

--- intensity_client.sh
+++ intensity_client.sh
@@ -1,2 +1,6 @@
+# Do a chdir to the dir with this script in it
+abspath="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
+path_only=`dirname "$abspath"`
+cd ${path_only}
 ./cbuild/src/client/Intensity_CClient -r $@
 
