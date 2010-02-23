07acf002735f603a068655b019ffae8466efcf88
Alon Zakai <alon@sanctity.(none)>
2010-02-23 11:41:40 +0200

    docs

--- src/python/intensity/__init__.py
+++ src/python/intensity/__init__.py
@@ -3,19 +3,10 @@
 
 ## \mainpage Intensity Engine API
 ##
-## This is the documentation for the Intensity Engine Python API. It is available online
-## <a href="http://docs.intensityengine.com/">here</a>. You can download it for offline viewing
-## <a href="http://www.intensityengine.com/download">here</a>.
+## This is the documentation for the Intensity Engine Python code. Note that this is
+## just useful internally in the engine, the actual API for development is in
+## JavaScript.
 ##
-## Copyright (C) 2008 Alon Zakai ('Kripken'). Available under the terms of the <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Attribution-Share Alike 2.0</a> license.
-##
-## You can submit feedback to the online versions of these pages by using the feedback forms at the bottom of each
-## page. We appreciate your helping us improve the documentation by doing so.
-##
-## Note that it is by far recommended to
-## <b>not</b> start by reading this documentation; you would almost certainly be better off
-## starting with the <a href="http://sites.google.com/a/intensityengine.com/intensity-engine/Developer-Documentation">'hello world'
-## example and other tutorials</a>, to get an overview and
-## grasp of the main concepts before getting to specifics.
+## Copyright (C) 2010 Alon Zakai ('Kripken'). Available under the terms of the <a href="http://creativecommons.org/licenses/by-sa/2.0/">Creative Commons Attribution-Share Alike 2.0</a> license.
 ##
 
