5d693f8529a4f7c63ce8e63ea9fe4c4ad042741f
Alon Zakai <alon@sanctity.(none)>
2010-04-09 16:08:53 +0300

    Hid MochiKit debugging stuff on client

--- src/javascript/MochiKit.js
+++ src/javascript/MochiKit.js
@@ -844,9 +844,9 @@ MochiKit.Base.update(MochiKit.Base, {
 
     /** @id MochiKit.Base.evalJSON */
     evalJSON: function (jsonText) {
-log(ERROR, "going to parse:" + jsonText);
+if (Global.SERVER) log(ERROR, "going to parse:" + jsonText);
 var ret = JSON.parse('(' + jsonText + ')');
-log(ERROR, "parsed ok");
+if (Global.SERVER) log(ERROR, "parsed ok");
 return ret;
 //        return JSON.parse("(" + jsonText + ")"); // INTENSITY: Had MochiKit.Base._filterJSON(jsonText)... XXX
 //        return eval("(" + MochiKit.Base._filterJSON(jsonText) + ")");
